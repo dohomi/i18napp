86aba495ca711446f5c0f07171eeac91a590c760
Dominic Garms <djgarms@gmail.com>
2015-02-19 08:04:23 +0800

    Updated source

--- i18napp.js
+++ i18napp.js
@@ -16,7 +16,11 @@ Router.configure({
     },
     getLanguage: function () {
 
-      return Session.get("lang");
+      if (Meteor.loggingIn() || !Meteor.userId()) {
+        return;
+      }
+
+      return Meteor.user() && Meteor.user().languageKey;
     }
   }
 });
@@ -61,6 +65,41 @@ if (Meteor.isClient) {
 
       Router.setLanguage($(e.currentTarget).val());
     }
-  })
+  });
 
 }
+
+
+if (Meteor.isServer) {
+  Meteor.publish(null, function () {
+    return Meteor.users.find(this.userId, {fields: {profile: 1, languageKey: 1}});
+  });
+}
+
+Meteor.startup(function () {
+
+  if (Meteor.isServer) {
+
+
+    var testuser = Meteor.users.findOne({username: "testuser@example.com"}), userId;
+
+    // create operator
+    if (!testuser) {
+
+      userId = Accounts.createUser({
+
+        username: "testuser@example.com",
+        email: "testuser@example.com",
+        password: "123123",
+        profile: {
+          firstName: "Test",
+          lastName: "User"
+        }
+      });
+      Meteor.users.update(userId, {$set: {languageKey: 'de'}});
+
+
+    }
+  }
+
+});
