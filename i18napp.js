011d7be3c76c12eb97759e0004e121fe766b955a
Dominic Garms <djgarms@gmail.com>
2015-02-23 21:46:51 +0800

    include getLangCode()

--- i18napp.js
+++ i18napp.js
@@ -1,22 +1,5 @@
 if (Meteor.isClient) {
 
-  Template.layout.rendered = function () {
-
-
-    this.autorun(function () {
-      var userLang = Meteor.user() && Meteor.user().languageKey;
-      if (!Meteor.loggingIn() && userLang) {
-        userLangKey.set(userLang);
-      }
-      //console.log(langCodeMissing,"langCodeMissing")
-      if ( Router.getLanguage() !== Helpers.language()) {
-        Helpers.setLanguage(Router.getLanguage());
-      }
-    });
-
-  };
-
-
   var events = {
     "click button": function (e) {
       var where = $(e.target).data("where"); //should be home or foo
@@ -25,25 +8,6 @@ if (Meteor.isClient) {
   };
   Template.Foo.events(events);
   Template.Home.events(events);
-
-
-  //util stuff to change language and show current one
-  Template.layout.helpers({
-    "lang": function () {
-      return Helpers.language();
-    },
-    "languages": function () {
-      return languages;
-    },
-    "isLang": function () {
-      return this == Helpers.language();
-    }
-  });
-  Template.layout.events({
-    "change select": function (e) {
-      Router.setLanguage($(e.currentTarget).val());
-    }
-  });
 
 }
 
