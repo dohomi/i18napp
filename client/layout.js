9eca2851b1a7a66f67c6d1683caaf728645bdc81
Dominic Garms <djgarms@gmail.com>
2015-02-23 21:49:33 +0800

    add missing files

---
+++ client/layout.js
@@ -0,0 +1,37 @@
+Template.layout.rendered = function () {
+
+
+  this.autorun(function () {
+    var initialLangCode = Router.getLangCode();
+    console.log("initialLangCode",initialLangCode)
+    var userLang = Meteor.user() && Meteor.user().languageKey;
+    // here I need to do an action, if no routing is set
+    if (!initialLangCode && !Meteor.loggingIn() && userLang) {
+      Router.setLanguage(userLang);
+    }
+    //console.log(langCodeMissing,"langCodeMissing")
+    if (Router.getLanguage() !== Helpers.language()) {
+      Helpers.setLanguage(Router.getLanguage());
+    }
+  });
+
+};
+
+//util stuff to change language and show current one
+Template.layout.helpers({
+  "lang": function () {
+    return Helpers.language();
+  },
+  "languages": function () {
+    return languages;
+  },
+  "isLang": function () {
+    return this == Helpers.language();
+  }
+});
+
+Template.layout.events({
+  "change select": function (e) {
+    Router.setLanguage($(e.currentTarget).val());
+  }
+});
